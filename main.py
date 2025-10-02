c6c34a509c78a9c325fa7840a18c7d8cd0e16483
Ellaine Dela Cruz <ellegdlcrz@gmail.com>
2025-10-02 21:26:36 +0800

    style: fix linting issues

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import air
 
 app = air.Air()
 
+
 @app.get("/")
 async def index():
     return air.layouts.mvpcss(
-        air.H1("Voices of PyLadies Manila"),
-        air.P("Coming soon...")
+        air.H1("Voices of PyLadies Manila"), air.P("Coming soon...")
     )
